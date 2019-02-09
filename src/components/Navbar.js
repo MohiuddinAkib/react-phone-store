4593b4c882751cf2eeca1e19eedcb902e0b8ee9c
mohammad mohiuddin mostafa kamal akib <mohiuddinmostafakamal@gmail.com>
2019-02-09 15:01:15 +0600

    created Button, Title components. Installed and applied styled-components module

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "../logo.svg";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark px-sm-5 bg-primary">
+    <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
       <Link to="/">
         <img src={logo} alt="logo" className="navbar-brand" />
       </Link>
@@ -20,15 +22,26 @@ const Navbar = () => {
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
           <Link to="/cart" className="nav-link">
-            <button>
-              <i className="fas fa-cart-plus" />
-              My Cart
-            </button>
+            <ButtonContainer>
+              <span className="mr-2">
+                <i className="fas fa-cart-plus" />
+              </span>
+              my cart
+            </ButtonContainer>
           </Link>
         </li>
       </ul>
-    </nav>
+    </NavWrapper>
   );
 };
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize !important;
+  }
+`;
 
 export default Navbar;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
 import Product from "./Product";
+import Title from "./Title";
+import { storeProducts } from "../data";
 
 class ProductList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: storeProducts
+    };
+  }
+
   render() {
     return (
-      <section>
-        <h3>Hello from product list</h3>
-        <Product />
+      <section className="py-5">
+        <div className="container">
+          <Title name="our" title="products" />
+          <div className="row">
+            <div className="col-md-4">
+              <Product />
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
